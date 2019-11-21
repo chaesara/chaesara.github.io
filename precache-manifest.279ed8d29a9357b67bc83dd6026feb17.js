5b1c1e0ab01b8916a21d5e1a1e386f86b3056ec8
chaesara <egalearner@gmail.com>
2019-11-21 14:46:12 +0700

    pwa hore

---
+++ precache-manifest.279ed8d29a9357b67bc83dd6026feb17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2468ce4834c1ac97f014",
+    "url": "/css/chunk-vendors.325972d5.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ef6f7c898a8d70a65e8540b6b2c8abb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be91c1d46c6a410fefc",
+    "url": "/js/app.ad941375.js"
+  },
+  {
+    "revision": "2468ce4834c1ac97f014",
+    "url": "/js/chunk-vendors.e6d33e08.js"
+  },
+  {
+    "revision": "b457ce115dbd9adf9f6230a812a92446",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
